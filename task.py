23a95c3f1455513f3cd622ef7f2d6e6864895659
Devon Ankar <devon.ankar@gmail.com>
2022-08-14 15:42:20 -0400

    Add sell opportunities and reformat

--- task.py
+++ task.py
@@ -72,60 +72,76 @@ def merge_fte_and_pi(pi_data: pd.DataFrame, chamber: str) -> pd.DataFrame:
             .drop(columns=['state'])
             .rename(columns=dict(winner_Dparty='fteD', winner_Rparty='fteR'))
     )
-    merged['side'] = 'buy'
     return merged
 
 
-def add_profit_columns_to_merged(merged: pd.DataFrame) -> pd.DataFrame:
-    buy_columns = ('bestBuyYesCostD', 'bestBuyNoCostR', 'bestBuyYesCostR', 'bestBuyNoCostD')
-
+def add_profit_columns(merged: pd.DataFrame) -> None:
     merged['profit_bestBuyYesCostD'] = merged.fteD - merged.bestBuyYesCostD
     merged['profit_bestBuyNoCostR'] = merged.fteD - merged.bestBuyNoCostR
     merged['profit_bestBuyYesCostR'] = merged.fteR - merged.bestBuyYesCostR
     merged['profit_bestBuyNoCostD'] = merged.fteR - merged.bestBuyNoCostD
 
-    for col in buy_columns:
-        # merged[f'roi{col}'] = (merged[f'profit{col}'] / merged[col]).fillna(0).apply(lambda x: int(round(x * 100)))
-        merged[f'profit_{col}'] = merged[f'profit_{col}'].round(2)
+    merged['profit_bestSellYesCostD'] = merged.bestSellYesCostD - merged.fteD
+    merged['profit_bestSellNoCostR'] = merged.bestSellNoCostR - merged.fteD
+    merged['profit_bestSellYesCostR'] = merged.bestSellYesCostR - merged.fteR
+    merged['profit_bestSellNoCostD'] = merged.bestSellNoCostD - merged.fteR
 
-    for col in ('fteD', 'fteR'):
-        merged[col] = merged[col].round(2)
 
+def add_action_columns(merged: pd.DataFrame, side: str) -> pd.DataFrame:
+    cost_columns = list(map(lambda x: x.format(side.title()), (
+        'best{}YesCostD', 'best{}NoCostR', 'best{}YesCostR', 'best{}NoCostD')))
     merged = merged.reset_index(drop=True)
-    transposed = merged[[f'profit_{i}' for i in buy_columns]].transpose()
-    addnl = [dict(
-        buyActionRec=re.search('profit_best(Buy)(Yes|No)Cost([DR])', transposed[i].idxmax()),
-        buyActionProfit=transposed[i].max(),
-    ) for i in transposed]
+    transposed = merged[[f'profit_{i}' for i in cost_columns]].transpose()
+    addnl = [{
+        'actionRec': re.search('profit_best(Buy|Sell)(Yes|No)Cost([DR])', transposed[i].idxmax()),
+        'actionProfit': transposed[i].max(),
+    } for i in transposed]
 
     merged = merged.join(pd.DataFrame(addnl))
-    merged.buyActionRec = merged.buyActionRec.apply(lambda x: x.groups()).apply(
+    merged.actionRec = merged.actionRec.apply(lambda x: x.groups()).apply(
         lambda x: '{} {} on the {}'.format(x[0], x[1], dict(D='Democrat', R='Republican')[x[2]]))
-    merged = merged[merged.buyActionProfit >= _MIN_PROFIT_PER_SHARE].sort_values('buyActionProfit', ascending=False)
+    merged['actionSide'] = side
     return merged
 
 
 def create_fte_and_pi_comparison() -> pd.DataFrame:
     pi_data = get_pi_data()
     merged = pd.concat(merge_fte_and_pi(pi_data, chamber) for chamber in _CHAMBERS['names'])
-    merged = add_profit_columns_to_merged(merged)
+    add_profit_columns(merged)
+    merged = pd.concat(add_action_columns(merged, side) for side in ('buy', 'sell'))
+    merged = merged[merged.actionProfit >= _MIN_PROFIT_PER_SHARE].sort_values('actionSide').sort_values(
+        'actionProfit', ascending=False)
+
+    merged.fteD = merged.fteD.round(2)
+    merged.fteR = merged.fteR.round(2)
+    merged.actionProfit = merged.actionProfit.round(2)
+
     return merged
 
 
 def create_html_output(merged: pd.DataFrame) -> None:
-    summary = merged.groupby('buyActionRec', as_index=False).agg(dict(
-        murl='count', seat='</li><li>'.join)).sort_values(by='murl', ascending=False)
+    summary = merged.groupby('actionRec', as_index=False).agg(dict(
+        murl='count', seat=', '.join)).sort_values(by='murl', ascending=False)
     forecast_exp_title = _FORECAST_EXPRESSION[1:].title()
 
-    items = [open('templates/market_item.html').read().format(
-        **i, forecast_expression=forecast_exp_title) for i in merged.to_dict('records')]
+    market_item_template = open('templates/market_item.html').read()
+    market_item_costs_template = open('templates/market_item_costs.html').read()
+    summary_item_template = open('templates/summary_item.html').read()
+
+    items = [
+        market_item_template.format(
+            forecast_expression=forecast_exp_title,
+            costs=market_item_costs_template.format(actionSideTitle=i['actionSide'].title()).format(**i),
+            **i,
+        ) for i in merged.to_dict('records')
+    ]
     items.insert(5, open('templates/notes_item.html').read().format(
         forecast_expression=forecast_exp_title, min_profit_per_share=_MIN_PROFIT_PER_SHARE))
     links_idx = 9
     html = open('templates/page.html').read().format(
         data0='\n'.join(items[:links_idx]),
         data1='\n'.join(items[links_idx:]),
-        summary='\n'.join(open('templates/summary_item.html').read().format(**i) for i in summary.to_dict('records')),
+        summary='\n'.join(summary_item_template.format(**i) for i in summary.to_dict('records')),
         update_interval='hourly',
         last_updated=datetime.now().strftime('%d %B %Y %H:%M'),
     )
